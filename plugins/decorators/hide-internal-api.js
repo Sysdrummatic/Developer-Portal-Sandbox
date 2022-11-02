a75e078ae0dfb9b81a5e5ec9a6cf6a59baf88704
LukaszMichta <lukasz.michta@cdq.com>
2022-11-02 12:17:54 +0100

    hide-internal-apis - Adjusting the plugin

--- plugins/decorators/hide-internal-api.js
+++ plugins/decorators/hide-internal-api.js
@@ -1,30 +1,21 @@
 module.exports = HideInternal;
 
 /** @type {import('@redocly/openapi-cli').OasDecorator} */
-
+/**
+ @returns {import("@redocly/openapi-core/lib/visitors").Oas3Visitor}
+ */
 function HideInternal() {
     return {
-        // DefinitionRoot: {
-        //     leave(ctx) {
-        //         if (ctx.hasOwnProperty("x-cdq-hide")) {
-        //             if (ctx["x-cdq-security"] === true) {
-        //                 ctx['security'] = [{'oAuth2': []}]
-        //                 ctx['components']['securitySchemes'] =
-        //                     {
-        //                         'oAuth2': {
-        //                             'type': 'oauth2',
-        //                             "flows": {
-        //                                 "clientCredentials": {
-        //                                     "x-tokenEndpointAuthMethod": "client_secret_basic",
-        //                                     "tokenUrl": "https://id.cdq.com/auth/realms/cs/protocol/openid-connect/token",
-        //                                     "scopes": {}
-        //                                 }
-        //                             }
-        //                         }
-        //                     }
-        //             }
-        //         }
-        //     }
-        // }
+        DefinitionRoot: {
+            leave(ctx) {
+                if (ctx.parameter.hasOwnProperty("x-cdq-internal")) {
+                    if (ctx['x-cdq-internal'] === true) {
+                        ctx['x-public'] = ["false"]
+                        ctx['x-hide'] = ["true"]
+
+                    }
+                }
+            }
+        }
     }
 };
